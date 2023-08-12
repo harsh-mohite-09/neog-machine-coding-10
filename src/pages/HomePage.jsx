4c347c38e0cd94917a1e8ca03d1d68f6459732f8
Harsh Mohite <harsh.mohite009@gmail.com>
2023-08-12 16:28:29 +0530

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Heading } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return (
+    <Flex justify="center">
+      <Heading mt={10}>MCR - 10</Heading>
+    </Flex>
+  );
+};
+
+export default HomePage;
